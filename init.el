c25b1866dafb92309c572dc8a69a9ce7a6a0867c
yaozhanliang <ivws02@126.com>
2015-05-02 08:57:31 +0800

    commit

--- init.el
+++ init.el
@@ -188,8 +188,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-clang-arguments (quote ("-I/home/lyzh/data/switch_power_test")))
+ '(company-clang-arguments (quote ("")))
  '(company-clang-insert-arguments t)
+ '(company-dabbrev-code-time-limit 0.3)
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
